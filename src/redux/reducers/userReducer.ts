939ca2f76ef3a9ca7ba3e32ca7997d32d77ade07
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2025-09-16 16:34:16 -0500

    user Reducer

---
+++ src/redux/reducers/userReducer.ts
@@ -0,0 +1,42 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { UserType } from "../../types/UserType";
+import { LocationType } from "../../types/UserType";
+
+const initialState: UserType = {
+    id: 0,
+    username: '',
+    location: {
+        lat: 44.9560534624369,
+        lng: -93.16002444658359
+    },
+    userTime: {
+        day: 0,
+        hours: 0,
+    }
+}
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: initialState,
+    reducers: {
+        setUser(state, action: PayloadAction<UserType>) {
+            state.id = action.payload.id;
+            state.username = action.payload.username;
+        },
+        logoutUser(state) {
+            state.id = 0; state.username = '';
+        },
+        setUserLocation(state, action: PayloadAction<LocationType>) {
+            console.log('setUserLocation', action.payload);
+            state.location = action.payload;
+        }
+    }
+});
+
+export const {
+    setUser,
+    logoutUser,
+    setUserLocation,
+} = userSlice.actions;
+
+export default userSlice.reducer;
